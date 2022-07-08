9c41ed97f5c5fd0202bc33e5a55637fec03a417b
Julia Pokrovskaya <camomile0@yandex.ru>
2022-07-08 22:12:15 +0300

    Add Ex4 and Ex5

---
+++ Ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = -number; i <=number ; i++)
+{
+ Console.WriteLine(i);  
+}
+
+
+
+
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int i = -1 * n;
+while (i <= number);
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+if i = 0;
+while (i <= number);
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
