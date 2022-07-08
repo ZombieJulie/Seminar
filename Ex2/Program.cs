a5e4b81d6980db91d5b43bc6de55b475917e3d74
Julia Pokrovskaya <camomile0@yandex.ru>
2022-07-08 21:38:58 +0300

    Add Ex2 with sqr and Ex3 with day of week

---
+++ Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB =  int.Parse(Console.ReadLine());
+if (numberB == numberA/numberB)
+{
+   Console.WriteLine("Верно");
+}
+else
+{
+Console.WriteLine("Не верно");
+}
+
